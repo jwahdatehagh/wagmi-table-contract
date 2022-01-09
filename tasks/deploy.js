b3173cfd3c7fe812e9ae3ae8516de19c135cfa56
jalil <jalil@1001.digital>
2022-01-09 16:19:11 +0100

    Add deployment tasks and configure a local hh network

---
+++ tasks/deploy.js
@@ -0,0 +1,23 @@
+const TEN_MINUTES = 60 * 10
+
+task('deploy', 'Deploys the smart contract')
+  .setAction(async () => {
+    const saleStart = (await ethers.provider.getBlock('latest')).timestamp + TEN_MINUTES
+    const DEPLOYMENT_ARGUMENTS = [
+      // Number of tokens
+      process.env.NUMBER_OF_TOKENS,
+      // The default price for all tokens
+      process.env.DEFAULT_PRICE,
+      // The IPFS hash of the collection
+      process.env.IPFS_CONTENT_ID,
+      // When the sale should start
+      saleStart,
+      // The artist address
+      process.env.ARTIST_ADDRESS
+    ]
+
+    const WagmiTable = await ethers.getContractFactory('WagmiTable')
+    const contract = await WagmiTable.deploy(...DEPLOYMENT_ARGUMENTS)
+
+    console.log(`Deployed the contract with arguments [${DEPLOYMENT_ARGUMENTS.join(' ')}] at address "${contract.address}"`)
+  })
