5122b8f7f012d7cb18a9e3a7ad0340399d8c4f1a
jalil <jalil@1001.digital>
2022-01-09 05:25:32 +0100

    Implement the arraalto WAGMI table smart contracts :)

--- contracts/WagmiTable.sol
+++ contracts/WagmiTable.sol
@@ -1,26 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@1001-digital/erc721-extensions/contracts/WithAdditionalMints.sol";
+import "@1001-digital/erc721-extensions/contracts/WithSaleStart.sol";
 
-contract WagmiTable is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+contract WagmiTable is WithAdditionalMints, WithSaleStart {
+    bool public frozen;
 
-    constructor() ERC721("WagmiTable", "WT") {
-        // 
+    uint256 public defaultPrice;
+    mapping(uint256 => uint256) public priceForToken;
+
+    constructor(
+        uint256 _initialSupply,
+        uint256 _defaultPrice,
+        string memory _cid,
+        uint256 _time,
+        address _aaron
+    )
+        ERC721("WagmiTable", "WT")
+        WithIPFSMetaData(_cid)
+        WithLimitedSupply(_initialSupply)
+        WithSaleStart(_time)
+    {
+        defaultPrice = _defaultPrice;
+
+        _mint(_aaron, 0);
     }
 
-    function awardItem(address player)
-        public
-        returns (uint256)
+    function mint(uint256 _tokenId, address _to)
+        external payable
+        withinSupply(_tokenId)
+        meetsPrice(_tokenId)
+        afterSaleStart
     {
-        _tokenIds.increment();
+        _mint(_to, _tokenId);
+    }
 
-        uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
+    modifier withinSupply (uint256 _tokenId) {
+        require(_tokenId < totalSupply(), "Token not available to mint");
 
-        return newItemId;
+        _;
+    }
+
+    modifier meetsPrice (uint256 _tokenId) {
+        uint256 tokenPrice = priceForToken[_tokenId] > 0
+            ? priceForToken[_tokenId]
+            : defaultPrice;
+        require(msg.value >= tokenPrice, "Pay up, friend");
+
+        _;
+    }
+
+    function setTokenPrice(uint256 _tokenId, uint256 _price) external onlyOwner {
+        priceForToken[_tokenId] = _price;
+    }
+
+    function setTokenPrices(uint256[] memory _tokenIds, uint256 _price) external onlyOwner {
+        for (uint256 index = 0; index < _tokenIds.length; index++) {
+            priceForToken[_tokenIds[index]] = _price;
+        }
+    }
+
+    function freeze() external onlyOwner {
+        frozen = true;
+    }
+
+    modifier unfrozen() {
+        require(! frozen, "Metadata already frozen");
+
+        _;
+    }
+
+    function setCID(string memory _cid) external onlyOwner unfrozen {
+        _setCID(_cid);
+    }
+
+    function addTokens(string memory _cid, uint256 _count) public override onlyOwner unfrozen {
+        super.addTokens(_cid, _count);
     }
 }
---
+++ test/WagmiTable.js
@@ -0,0 +1,146 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+
+const DEFAULT_PRICE = ethers.utils.parseEther('0.2')
+const CONTENT_ID = 'FOOBARBAZ'
+const AARAALTO = '0x03C19A8432904f450cc90ba1892329deb43C8077'
+const TWO_MINUTES = 120
+
+describe('WagmiTable', function () {
+  let
+    WagmiTable,
+    contract,
+    owner,
+    buyer,
+    buyer2,
+    addrs
+
+  before(async () => {
+    WagmiTable = await ethers.getContractFactory('WagmiTable')
+  })
+
+  beforeEach(async () => {
+    saleStart = (await ethers.provider.getBlock('latest')).timestamp - TWO_MINUTES
+    contract = await WagmiTable.deploy(100, DEFAULT_PRICE, CONTENT_ID, saleStart, AARAALTO)
+    await contract.deployed();
+
+    // Get user addresses
+    [ owner, buyer, buyer2, ...addrs ] = await ethers.getSigners()
+  })
+
+  describe('Deployment', () => {
+    it('should set the initial total supply', async () => {
+      expect(await contract.totalSupply()).to.equal(100)
+    })
+
+    it('should mint the WAGMI token #0 to aarons address', async () => {
+      expect(await contract.ownerOf(0)).to.equal(AARAALTO)
+    })
+  })
+
+  describe('Token Prices', () => {
+    it('should set the default token price', async () => {
+      expect(await contract.defaultPrice()).to.equal(DEFAULT_PRICE)
+    })
+
+    it('should enable the owner to set a special price for a token', async () => {
+      const specialPrice = ethers.utils.parseEther('1.618')
+
+      await expect(contract.connect(owner).setTokenPrice(1, specialPrice))
+        .to.not.be.reverted
+
+      expect(await contract.priceForToken(1)).to.equal(specialPrice)
+    })
+
+    it('should enable the owner to set a special price for multiple tokens', async () => {
+      const specialPrice = ethers.utils.parseEther('1.618')
+
+      await expect(contract.connect(owner).setTokenPrices([1, 2, 10, 50], specialPrice))
+        .to.not.be.reverted
+
+      expect(await contract.priceForToken(1)).to.equal(specialPrice)
+      expect(await contract.priceForToken(2)).to.equal(specialPrice)
+      expect(await contract.priceForToken(10)).to.equal(specialPrice)
+      expect(await contract.priceForToken(50)).to.equal(specialPrice)
+    })
+  })
+
+  describe('Collection Metadata', () => {
+    it('allows the owner to update the collection metadata', async () => {
+      await expect(contract.connect(buyer).setCID('FOONEW'))
+        .to.be.revertedWith('Ownable: caller is not the owner')
+
+      await expect(contract.connect(owner).setCID('FOONEW'))
+        .to.emit(contract, 'MetadataURIChanged')
+        .withArgs(`ipfs://FOONEW`)
+    })
+
+    it('allows the owner to freeze the metadata', async () => {
+      await contract.connect(owner).freeze()
+
+      await expect(contract.connect(owner).setCID('FOONEW'))
+        .to.be.revertedWith('Metadata already frozen')
+    })
+
+    it('allows the owner to create additional supply', async () => {
+      await expect(contract.connect(owner).addToken('UPDATED_CID'))
+        .to.emit(contract, 'SupplyChanged')
+        .withArgs(101)
+    })
+
+    it('does not allow the owner to create additional supply when metadata is frozen', async () => {
+      await contract.connect(owner).freeze()
+
+      await expect(contract.connect(owner).addToken('UPDATED_CID'))
+        .to.be.revertedWith('Metadata already frozen')
+
+      await expect(contract.connect(owner).mintAdditionalTokens('UPDATED_CID', 5, owner.address))
+        .to.be.revertedWith('Metadata already frozen')
+    })
+  })
+
+  describe('Public Sale', () => {
+    it('should not allow users to mint tokens before the sale starts', async () => {
+      saleStart = (await ethers.provider.getBlock('latest')).timestamp + TWO_MINUTES
+      contract = await WagmiTable.deploy(100, DEFAULT_PRICE, CONTENT_ID, saleStart, AARAALTO)
+      await contract.deployed();
+
+      await expect(contract.connect(buyer).mint(1, buyer.address, { value: DEFAULT_PRICE }))
+        .to.be.revertedWith(`Sale hasn't started yet`)
+    })
+
+    it('should allow users to mint a token', async () => {
+      await expect(contract.connect(buyer).mint(1, buyer.address, { value: DEFAULT_PRICE }))
+        .to.emit(contract, 'Transfer')
+        .withArgs(ethers.constants.AddressZero, buyer.address, 1)
+    })
+
+    it('should not allow users to mint a token that is out of range', async () => {
+      await expect(contract.connect(buyer).mint(111, buyer.address))
+        .to.be.revertedWith(`Token not available to mint`)
+    })
+
+    it('should not allow users to mint tokens that are already sold', async () => {
+      await contract.connect(buyer).mint(50, buyer.address, { value: DEFAULT_PRICE })
+
+      await expect(contract.connect(buyer2).mint(50, buyer2.address, { value: DEFAULT_PRICE }))
+        .to.be.revertedWith(`ERC721: token already minted`)
+    })
+
+    it('should not allow to mint a token for less than its price', async () => {
+      await expect(contract.connect(buyer).mint(1, buyer.address))
+        .to.be.revertedWith(`Pay up, friend`)
+
+      await expect(contract.connect(buyer).mint(2, buyer.address, { value: DEFAULT_PRICE.sub(10) }))
+        .to.be.revertedWith(`Pay up, friend`)
+
+
+      const specialPrice = ethers.utils.parseEther('1.618')
+      await expect(contract.connect(owner).setTokenPrice(3, specialPrice))
+        .to.not.be.reverted
+
+      await expect(contract.connect(buyer).mint(3, buyer.address, { value: DEFAULT_PRICE }))
+        .to.be.revertedWith(`Pay up, friend`)
+    })
+  })
+})
