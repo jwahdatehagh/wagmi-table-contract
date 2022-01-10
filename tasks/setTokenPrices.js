6056a1060f1ed84ba87e39fbc8f6df3bfd866252
jalil <jalil@1001.digital>
2022-01-10 16:25:46 +0100

    Update the token prices

--- tasks/setTokenPrices.js
+++ tasks/setTokenPrices.js
@@ -2,7 +2,11 @@ const { nowInSeconds } = require('../helpers/time')
 
 task('setTokenPrices', 'Update the price for specific token IDs')
   .addParam('address', 'The contract address to update')
-  .addOptionalParam('tokenIds', 'The token IDs to update - comma separated', '1,2,18')
+  .addOptionalParam(
+    'tokenIds',
+    'The token IDs to update - comma separated',
+    '1,2,8,9,10,11,14,15,18,58,86,112,115'
+  )
   .addOptionalParam('price', 'The price for the tokens in WEI', '1618000000000000000')
   .setAction(async ({ address, tokenIds, price }) => {
     const WagmiTable = await ethers.getContractFactory('WagmiTable')
