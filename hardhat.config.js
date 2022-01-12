ac359e0eb549f52de001b5a8f4aba0d3db84319c
jalil <jalil@1001.digital>
2022-01-12 05:07:45 +0100

    Add compiler optimisation

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,15 @@ require('./tasks/setTokenPrices.js');
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.4',
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     mainnet: {
       url: process.env.MAINNET_URL || '',
